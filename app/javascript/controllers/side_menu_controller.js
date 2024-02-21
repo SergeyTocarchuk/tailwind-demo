4b115a78df8bd0cba526852e7a6ab7c0fef4d11e
Serhii T <tocarchuk@gmail.com>
2024-02-21 22:24:59 +0200

    wip add side menu

--- app/javascript/controllers/side_menu_controller.js
+++ app/javascript/controllers/side_menu_controller.js
@@ -3,12 +3,8 @@ import { Controller } from '@hotwired/stimulus'
 export default class extends Controller {
   
   static targets = ['sideMenu'];
-  
-  // connect() {
-
-  // }
 
   toggle() {
-    this.sideMenuTarget.classList.toggle('side-menu');
+    this.sideMenuTarget.classList.toggle('hide');
   }
 }
